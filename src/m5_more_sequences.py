5a6aa3990582cd469f56cd1383a72da8186b420b
myersjm <myersjm@rose-hulman.edu>
2017-09-27 20:33:32 -0400

    final commit -Jessica  Myers

--- src/m5_more_sequences.py
+++ src/m5_more_sequences.py
@@ -7,8 +7,8 @@ for ITERATING through SEQUENCES, including:
   -- The FIND pattern (via LINEAR SEARCH)
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Jessica Myers.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -75,7 +75,7 @@ def sum_radii(circles):
       :type circles:  list[rg.Circle]    or tuple(rg.Circle)
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #     The testing code is already written for you (above).
     #
     # Note: No fair using "slices" on ANY of these problems,
@@ -86,6 +86,11 @@ def sum_radii(circles):
     #
     #       Instead, use explicit loops, as you have for other problems.
     # ------------------------------------------------------------------
+    total = 0
+    for k in range(len(circles)):
+        circle = circles[k]
+        total = total + circle.radius
+    return total
 
 
 # ----------------------------------------------------------------------
@@ -149,9 +154,14 @@ def count_last_n_odds(integers, n):
       :type n:        int
     """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
+    total = 0
+    for k in range(len(integers) - n, len(integers)):
+        if integers[k] % 2 != 0:
+            total = total + 1
+    return total
 
 
 # ----------------------------------------------------------------------
@@ -223,9 +233,13 @@ def index_of_first_negative(numbers):
       :type integers: list[float]   or tuple[float]
     """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #     The testing code is already written for you (above).
     # ------------------------------------------------------------------
+    for k in range(len(numbers)):
+        if numbers[k] < 0:
+            return k
+    return -1
 
 
 def run_test_contains_an_a():
@@ -280,7 +294,7 @@ def contains_an_a(s):
       :type s: str
     """
     # ------------------------------------------------------------------
-    # TODO: 5. Implement and test this function.
+    # DONE: 5. Implement and test this function.
     #     The testing code is already written for you (above).
     #
     ####################################################################
@@ -293,6 +307,10 @@ def contains_an_a(s):
     #   Use an explicit loop, as you have done in the other problems.
     #   No fair using the   count   or   find   string methods.
     # ------------------------------------------------------------------
+    for k in range(len(s)):
+        if s[k] == 'a':
+            return True
+    return False
 
 
 # ----------------------------------------------------------------------
